e2d7056647b9ebfce122c96e8412676902289b5e
Albeoris <albeoris@gmail.com>
2020-09-09 00:09:48 +0300

    Initial Commit

---
+++ Albeoris.Framework/Collections/_IEnumerable_.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Albeoris.Framework.Collections
+{
+    // ReSharper disable once InconsistentNaming
+    public static class _IEnumerable_
+    {
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> self, params T[] values)
+        {
+            foreach (T item in self)
+                yield return item;
+            foreach (T item in values)
+                yield return item;
+        }
+    }
+}
---
+++ Albeoris.Framework/System/_StringBuilder_.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace Albeoris.Framework.System
+{
+    // ReSharper disable once UnusedType.Global
+    // ReSharper disable once InconsistentNaming
+    public static class _StringBuilder_
+    {
+        public static void Clear(this StringBuilder self)
+        {
+            self.Length = 0;
+        }
+        
+        public static StringBuilder AppendFormatLine(this StringBuilder self, String format, params Object[] args)
+        {
+            self.AppendFormat(format, args);
+            self.AppendLine();
+
+            return self;
+        }
+
+        public static Int32 IndexOf(this StringBuilder self, Int32 offset, Char ch)
+        {
+            for (Int32 i = offset; i < self.Length; i++)
+            {
+                if (self[i] == ch)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public static Int32 IndexFromEnd(this StringBuilder self, Int32 offset, Char ch)
+        {
+            for (Int32 i = offset; i >= 0; i--)
+            {
+                if (self[i] == ch)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public static String Evict(this StringBuilder self)
+        {
+            String result = self.ToString();
+            self.Length = 0;
+            return result;
+        }
+    }
+}
